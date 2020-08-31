04ead73f75be6533dfa081a76fc72b7b253de6b3
Maria <formacionjee@gmail.com>
2020-08-31 13:20:46 +0200

    Mapping methods & parameters

--- mvc/app/controllers/Pages.php
+++ mvc/app/controllers/Pages.php
@@ -3,7 +3,14 @@
 class Pages {
     
     public function __construct(){
-        echo 'Pages loaded';
+      
     }
 
+    public function index(){
+        
+    }
+
+    public function about($id){
+        echo 'this is about ' .$id;
+    }
 }
