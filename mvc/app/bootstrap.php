c23b10144f732278b2768149385055e2dd45e325
Maria <formacionjee@gmail.com>
2020-08-31 13:00:37 +0200

    Bootsrap file & core classes

--- mvc/app/bootstrap.php
+++ mvc/app/bootstrap.php
@@ -1,2 +1,6 @@
 <?php
- echo 'bootstraping';
+
+// Load libraries
+require_once 'libraries/Controller.php';
+require_once 'libraries/Core.php';
+require_once 'libraries/Database.php';
--- mvc/public/index.php
+++ mvc/public/index.php
@@ -2,3 +2,6 @@
 
 require_once '../app/bootstrap.php';
 
+// Init Core Library
+
+$init = new Core;
